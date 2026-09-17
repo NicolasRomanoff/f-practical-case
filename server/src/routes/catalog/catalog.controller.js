ed8b64a5e221ec6264b40d7b8a6053684d4b2faf
NicolasR <nicolas.romanoff94@gmail.com>
2026-09-17 13:40:29 +0200

    chore: delete comments

--- server/src/routes/catalog/catalog.controller.js
+++ server/src/routes/catalog/catalog.controller.js
@@ -1,9 +1,6 @@
 import { db } from "../../sqlite.js";
 
 export const getCatalog = (req, res) => {
-  // const type = req.query.type || "";
-  // const search = req.query.search || "";
-
   let sql = `
     SELECT
       MIN(d.id) AS id,
@@ -14,22 +11,8 @@ export const getCatalog = (req, res) => {
     WHERE d.owner_id IS NULL
     GROUP BY d.name, d.type
   `;
-  const params = [];
 
-  // if (type) {
-  //   sql += " AND d.type = ?";
-  //   params.push(type);
-  // }
-
-  // if (search) {
-  //   sql += " AND (LOWER(d.name) LIKE ? OR LOWER(d.type) LIKE ?)";
-  //   params.push(`%${search.toLowerCase()}%`);
-  //   params.push(`%${search.toLowerCase()}%`);
-  // }
-
-  // sql += " ORDER BY d.id DESC";
-
-  db.all(sql, params, (err, rows) => {
+  db.all(sql, [], (err, rows) => {
     if (err) {
       return res
         .status(500)
